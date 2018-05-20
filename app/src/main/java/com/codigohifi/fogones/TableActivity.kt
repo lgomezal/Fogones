d42d3737b6f417a46e1c417b1fde7e2763146784
luisgomezalonso <lgomezal@gmail.com>
2018-05-20 23:11:25 +0200

    Design bill screen

--- app/src/main/java/com/codigohifi/fogones/TableActivity.kt
+++ app/src/main/java/com/codigohifi/fogones/TableActivity.kt
@@ -6,12 +6,28 @@ import kotlinx.android.synthetic.main.activity_tables.*
 
 class TableActivity : AppCompatActivity() {
 
+    var table: Table? = null
+        set(value) {
+            if (value != null) {
+                table_image.setImageResource(value.icon)
+                table_description.text = value.description
+                persons.text = getString(R.string.persons_format, value.persons)
+
+                var paidString = "NO"
+                if (value.paidOut == true) {
+                    paidString = "SI"
+                }
+                paidOut.text = getString(R.string.paidOut_format, paidString)
+                bill.text = getString(R.string.bill_format, 100)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tables)
 
         // Creamos las mesas en dummy
-        val table1 = Table(
+        table = Table(
                 1,
                 "MESA 1",
                 4,
@@ -19,21 +35,7 @@ class TableActivity : AppCompatActivity() {
                  R.drawable.tableimage,
                 null)
 
-        setTable(table1)
 
     }
 
-    fun setTable(table: Table) {
-        table_image.setImageResource(table.icon)
-        table_description.text = table.description
-        persons.text = getString(R.string.persons_format, table.persons)
-
-        var paidString = "NO"
-        if (table.paidOut == true) {
-            paidString = "SI"
-        }
-        paidOut.text = getString(R.string.paidOut_format, paidString)
-        bill.text = getString(R.string.bill_format, 100)
-
-    }
 }
